bf80ff5fac6d3a6caeefa363c01cbc84f65a6771
matias96 <matias_sanchez96@yahoo.com>
2017-11-27 14:38:04 -0300

    Agregado vista registro y contraseña hasheada

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -63,6 +63,22 @@ class UsersController extends AppController
         $this->set(compact('user'));
         $this->set('_serialize', ['user']);
     }
+    
+    public function registro()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
+    }
 
     /**
      * Edit method
